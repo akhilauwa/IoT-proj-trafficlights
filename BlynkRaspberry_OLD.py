a2dabdd628c14c0b2c2dce0cd759eb43f7c6909f
akhilauwa <23729213@student.uwa.edu.au>
2023-09-25 16:10:05 +0800

    created VM version and renamed files

---
+++ BlynkRaspberry_OLD.py
@@ -0,0 +1,84 @@
+#define BLYNK_TEMPLATE_ID "TMPL6lRXOjBIZ"
+#define BLYNK_TEMPLATE_NAME "RaspberryPi IoT"
+#define BLYNK_DEVICE_NAME "RaspberryPi"
+
+# install Blynk library on Raspberry Pi
+# pip install blynklib-rpi -- upgrade
+
+# may need these
+# sudo apt-get update
+# sudo apt-get install python-dev python-pip libssl-dev libffi-dev
+
+
+import blynklib
+import time
+import RPi.GPIO as GPIO
+
+BLYNK_AUTH_TOKEN = "Bm7q73fxUgn88ztlTApzlL1ReDDSK68j"
+
+# Initialize Blynk 
+blynk = blynklib.Blynk(BLYNK_AUTH_TOKEN)
+
+# Set up GPIO pins
+led1_pin = 17
+led1_virt_pin = 0
+
+led2_pin = 23
+led2_virt_pin = 1
+
+pot_pin = 34
+pot_virt_pin = 2
+
+term_virt_pin = 3
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(pot_pin, GPIO.IN)
+
+# TODO FIX PIN NAMES
+# Register virtual pin handler for V0
+@blynk.on("writeV0")
+def v0_write_handler(pin, value):
+    if int(value[0]) == 1:
+        # Turn on LED1
+        GPIO.output(led1_pin, GPIO.HIGH)
+    else:
+        # Turn off LED1
+        GPIO.output(led1_pin, GPIO.LOW)
+
+# Register virtual pin handler for V1
+@blynk.on("writeV1")
+def v1_write_handler(pin, value):
+    if int(value[0]) == 1:
+        # Turn on LED2
+        GPIO.output(led2_pin, GPIO.HIGH)
+    else:
+        # Turn off LED2
+        GPIO.output(led2_pin, GPIO.LOW)
+
+@blynk.on("connected")
+def blynk_connected(ping):
+    print('Blynk ready. Ping:', ping, 'ms')
+    # You can also use blynk.sync_virtual(pin)
+
+@blynk.on("disconnected")
+def blynk_disconnected():
+    print('Blynk disconnected')
+
+while True:
+    try:
+        potvalue = GPIO.input(pot_pin)
+        print("pot =", potvalue)
+        blynk.virtual_write(pot_virt_pin, potvalue)
+
+        terminalStr = "pot = {}".format(potvalue)
+        print(terminalStr)
+        blynk.virtual_write(term_virt_pin, terminalStr)
+
+        time.sleep(1)
+
+        blynk.run()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        break
--- BlynkRaspberry_VM.py
+++ BlynkRaspberry_VM.py
@@ -1,25 +1,10 @@
-#define BLYNK_TEMPLATE_ID "TMPL6lRXOjBIZ"
-#define BLYNK_TEMPLATE_NAME "RaspberryPi IoT"
-#define BLYNK_DEVICE_NAME "RaspberryPi"
-
-# install Blynk library on Raspberry Pi
-# pip install blynklib-rpi -- upgrade
-
-# may need these
-# sudo apt-get update
-# sudo apt-get install python-dev python-pip libssl-dev libffi-dev
-
-
-import blynklib
+import BlynkLib
 import random
 import time
 # import RPi.GPIO as GPIO
 
-# BLYNK_AUTH_TOKEN = "Bm7q73fxUgn88ztlTApzlL1ReDDSK68j"
-BLYNK_AUTH_TOKEN = "jw1NZLXq38HoD_0YzrBQ2xcAIPY59nip"
-
 # Initialize Blynk
-blynk = blynklib.Blynk(BLYNK_AUTH_TOKEN)
+blynk = BlynkLib.Blynk('jw1NZLXq38HoD_0YzrBQ2xcAIPY59nip')
 
 # Set up GPIO pins
 led1_pin = 17
@@ -35,73 +20,51 @@ term_virt_pin = 3
 
 potvalue = 0
 
-WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
-READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(17, GPIO.OUT)
+# GPIO.setup(23, GPIO.OUT)
 
-# Register virtual pin handler for V0
-@blynk.handle_event("write V0")
-def v0_write_handler(pin, value):
-    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
-    if value == 1:
+# Register Virtual Pins
+@blynk.VIRTUAL_WRITE(0)
+def my_write_handler(value):
+    print('Current V0 value: ')
+    print(value)
+    if value == ['1']:
         # Turn on LED1
-        GPIO.output(led1_pin, GPIO.HIGH)
+        # GPIO.output(led1_pin, GPIO.HIGH)
         print("LED1 On")
     else:
         # Turn off LED1
-        GPIO.output(led1_pin, GPIO.LOW)
+        # GPIO.output(led1_pin, GPIO.LOW)
         print("LED1 Off")
 
-
-# Register virtual pin handler for V1
-@blynk.handle_event("write V1")
-def v1_write_handler(pin, value):
-    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
-    if value == 1:
-        # Turn on LED2
-        GPIO.output(led2_pin, GPIO.HIGH)
+# Register Virtual Pins
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+    print('Current V1 value: ')
+    print(value)
+    if value == ['1']:
+        # Turn on LED1
+        # GPIO.output(led2_pin, GPIO.HIGH)
         print("LED2 On")
     else:
-        # Turn off LED2
-        GPIO.output(led2_pin, GPIO.LOW)
+        # Turn off LED1
+        # GPIO.output(led2_pin, GPIO.LOW)
         print("LED2 Off")
 
-
-@blynk.handle_event("read V2")
-def v2_read_handler(pin):
-    print(READ_PRINT_MSG.format(pin))
-    # blynk.virtual_write(pin, potvalue)
-    blynk.virtual_write(pin, random.randint(0, 5000))
-    
-
-CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
-DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
-
-
-@blynk.handle_event("connect")
-def connect_handler():
-    print(CONNECT_PRINT_MSG)
-    print('Sleeping 2 sec in connect handler...')
-    time.sleep(2)
-    # blynk.disconnect()
-
-
-@blynk.handle_event("disconnect")
-def disconnect_handler():
-    print(DISCONNECT_PRINT_MSG)
-    print('Sleeping 4 sec in disconnect handler...')
-    time.sleep(4)
-
+@blynk.VIRTUAL_READ(2)
+def my_read_handler():
+    # this widget will show some time in seconds..
+    blynk.virtual_write(2, random.randint(0, 5000))
 
 while True:
-    try: 
-#         potvalue += 1
-#         if potvalue > 4900:
-#             potvalue = 0
+    try:
+        potvalue += 1
+        if potvalue > 4900:
+            potvalue = 0
 #         print("pot = ", potvalue)
         # blynk.virtual_write(pot_virt_pin, potvalue)
-        # time.sleep(1)
-
         blynk.run()
     except KeyboardInterrupt:
-        GPIO.cleanup()
+        # GPIO.cleanup()
         break
